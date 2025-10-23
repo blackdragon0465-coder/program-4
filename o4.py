22bf0700f6d6a021723ca56aeed37205074d246f
blackdragon <blackdragon0465@gmail.com>
2025-10-23 16:54:26 +0530

    update code

--- o4.py
+++ o4.py
@@ -1,4 +1,28 @@
- nums=[2,3,4,5,6,7,8,9,10]
- e=sum(n%2==0 for n in nums)
- o=sum(n%2 for n in nums)
- print(f"Even:{e} Odd:{o}")
+# even_odd_counter.py
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_count = 0
+odd_count = 0
+prime_count = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+    if is_prime(num):
+        prime_count += 1
+
+print("Even numbers:", even_count)
+print("Odd numbers:", odd_count)
+print("Prime numbers:", prime_count)
